a0beaca011d24e77cd00de037d01d5f48c357e5b
Tom-Oram <e8martint@gmail.com>
2025-10-22 23:53:00 +0100

    added pygame init

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while:
+        screen = fill()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
